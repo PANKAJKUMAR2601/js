43e127403622c732e3422daeee6897782870ede5
PANKAJKUMAR2601 <PANKAJKUMAR1767361@GMAIL.COM>
2026-04-02 11:15:10 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+// how to declare varuiable through constant 
+const accountId = 144553
+let accountEmail = "pankajkumargoogle.com"
+var accountPassword =  "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 // not allowed 
+
+console.log(accountId);
