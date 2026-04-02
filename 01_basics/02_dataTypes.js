aca306654cd9168a3021949d59aa62ca26cd51cd
PANKAJKUMAR2601 <PANKAJKUMAR1767361@GMAIL.COM>
2026-04-02 20:50:34 +0000

    container config done

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,29 +1,44 @@
-"use strict"; // treat all JS code as newer version
+// "use strict"; // treat all JS code as newer version
 
-// alert( 3 + 3) // we are using nodejs, not browser
+// // alert( 3 + 3) // we are using nodejs, not browser
 
-console.log(3 
-    +
-     3) // code readability should be high
+// console.log(3 
+//     +
+//      3) // code readability should be high
 
-console.log("pankaj kumar  ")
-
-
-let name = "pankaj kumar"
-let age = 20
-let isLoggedIn = false
-let state;
-
-// number => 2 to power 53
-// bigint
-// string => ""
-// boolean => true/false
-// null => standalone value
-// undefined => 
-// symbol => unique
+// console.log("pankaj kumar  ")
 
 
-// object
+// let name = "pankaj kumar"
+// let age = 20
+// let isLoggedIn = false
+// let state;
 
-console.log(typeof undefined); // undefined
-console.log(typeof null); // object
+// // number => 2 to power 53
+// // bigint
+// // string => ""
+// // boolean => true/false
+// // null => standalone value
+// // undefined => 
+// // symbol => unique
+
+
+// // object
+
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
+
+
+"use strict";
+
+let name = "Pankaj";
+let age = 20;
+let isLoggedIn = true;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Logged In:", isLoggedIn);
+
+console.log("Type of name:", typeof name);
+console.log("Type of age:", typeof age);
+console.log("Type of login:", typeof isLoggedIn);
